d5f64f1d2ed951e4e5d64f55ec58c2d871690ba4
sandeep vadlapudi <vadlapudisanndeep@gmail.com>
2023-08-23 09:21:46 +0530

    Strings

---
+++ stringSlicing.py
@@ -0,0 +1,17 @@
+fruit = "Pineapple"
+
+# displays all characters
+print(fruit[:])
+# displays all characters from 0 to 0 1 is excluded
+print(fruit[:1])
+# displays all characters from 0 to 1 2 is excluded
+print(fruit[:2])
+
+print(fruit[1:3])
+
+print(fruit[4:7])
+
+len_fruit = len(fruit)
+print('len_fruit', len_fruit)
+
+print(fruit[-5:-1])
---
+++ strings.py
@@ -0,0 +1,28 @@
+# String is sequence of characters
+# it can be accessed using index
+name = "Hello!!"
+another_name = "Sandeep"
+
+print("hello!!" + another_name)
+print(name + another_name)
+
+number = 101
+print(str(number) + another_name)
+
+# Accessing characters of a string
+print(another_name[0])
+print(another_name[6])
+
+# Looping through the string
+for chars in name:
+    print(chars)
+
+# long_text = "hi sandeep
+#              where are you
+#               Am at Home"
+long_text = '''hi sandeep
+where are you 
+Am at Home '''
+print(long_text)
+
+
