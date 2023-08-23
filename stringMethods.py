4c8fec3ac3d992d874f2fb2a4d5b8160515876de
sandeep vadlapudi <vadlapudisanndeep@gmail.com>
2023-08-23 09:54:27 +0530

    string methods

---
+++ stringMethods.py
@@ -0,0 +1,32 @@
+Name = "learning Python for automation"
+
+print(Name.title())
+
+print(Name.swapcase())
+
+print(Name.lower())
+
+print(Name.upper())
+
+print(Name.capitalize())
+
+print(Name.split(" "))
+
+print(Name.index("l"))
+
+
+text ="  space   text   !!"
+
+print("remove_white_spaces",text.replace(" ",""))
+
+print('strip =',text.strip())
+print('text==',text.lstrip())
+
+print('text==',text.rstrip())
+
+print(text.count("!"))
+
+print(text.isspace())
+alphaNumber = "sandy@123"
+
+print(alphaNumber.isalnum())
